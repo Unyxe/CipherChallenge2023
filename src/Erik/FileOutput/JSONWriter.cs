2369e3b7232fffa65ed1bd948249273f349f23ce
ErikPeter2000 <69848038+ErikPeter2000@users.noreply.github.com>
2023-11-24 10:33:01 +0000

    FileOutput

--- src/Erik/FileOutput/JSONWriter.cs
+++ src/Erik/FileOutput/JSONWriter.cs
@@ -3,37 +3,40 @@ using System.Xml;
 using System.Xml.Serialization;
 
 namespace FileOutput;
-public static class JSONWriter
+public class JSONWriter
 {
-    /// <summary>
-    /// Writes the results of a cipher cracking to a file.
-    /// </summary>
-    /// <param name="path">The .json file path to write to.</param>
-    /// <param name="data"></param>
-    public static void WriteToFile(string path, CipherFileData data)
+    private bool promptYesNo(string message)
     {
-        if (!File.Exists(path))
-        {
-            FileInfo file = new FileInfo(path);
-            file.Directory?.Create();
-        }
-        string jsonString = JsonSerializer.Serialize(data);
-        using (StreamWriter sw = new StreamWriter(path))
-        {
-            sw.Write(jsonString);
-        }
+        Console.Write(message);
+        bool resp = Console.ReadLine()?.ToLower() == "y";
+        return resp;
     }
     /// <summary>
     /// Writes the results of a cipher cracking to a file.
     /// </summary>
     /// <param name="path">The .json file path to write to.</param>
-    /// <param name="ciphertext"></param>
-    /// <param name="key"></param>
-    /// <param name="plaintext"></param>
-    /// <param name="cipherName">A string name of the cipher.</param>
-    public static void WriteToFile(string path, string ciphertext, string key, string plaintext, string cipherName)
+    /// <param name="data"></param>
+    public void WriteToFile(string path, CipherFileData data, bool silent)
     {
-        var data = new CipherFileData(ciphertext, key, plaintext, cipherName);
-        WriteToFile(path, data);
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            FileInfo file = new FileInfo(path);
+            // confirm create directory
+            if (!(silent || promptYesNo($"Create directory: {file.DirectoryName}? [Y/N] ")))
+                return;
+            file.Directory?.Create();
+        }
+
+        // confirm overwrite file
+        if (File.Exists(path))
+            if (!(silent || promptYesNo($"Overwrite file: {path}? [Y/N] ")))
+                return;
+
+        string jsonString = JsonSerializer.Serialize(data);
+        using (StreamWriter sw = new StreamWriter(path))
+            sw.Write(jsonString);
+
+        if (!silent)
+            Console.WriteLine($"Output to file: {path}");
     }
 }
